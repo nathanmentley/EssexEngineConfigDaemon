2f9100190355902737558af179395e720b5b42a5
Nathan Mentley <nathanmentley@gmail.com>
2017-09-17 16:36:26 -0700

    init

---
+++ ConfigDaemon.cpp
@@ -0,0 +1,43 @@
+/* 
+ * Essex Engine
+ * 
+ * Copyright (C) 2017 Nathan Mentley - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the BSD license.
+ *
+ * You should have received a copy of the BSD license with
+ * this file. If not, please visit: https://github.com/nathanmentley/EssexEngine
+ */
+
+#include <EssexEngineConfigDaemon/ConfigDaemon.h>
+
+EssexEngine::Daemons::Config::ConfigDaemon::ConfigDaemon(WeakPointer<Context> _context):BaseDaemon(_context) {
+	build_number = 12;
+	build_name = "build name";
+}
+
+EssexEngine::Daemons::Config::ConfigDaemon::~ConfigDaemon() {}
+
+extern "C" {
+    void daemon_init(EssexEngine::Context* context) {
+        context->RegisterDaemon<EssexEngine::Daemons::Config::ConfigDaemon>(
+            new EssexEngine::Daemons::Config::ConfigDaemon(context)
+        );
+    }
+}
+
+int EssexEngine::Daemons::Config::ConfigDaemon::GetBuildNumber() {
+	return build_number;
+}
+
+std::string EssexEngine::Daemons::Config::ConfigDaemon::GetBuildName() {
+	return build_name;
+}
+
+int EssexEngine::Daemons::Config::ConfigDaemon::GetScreenWidth() {
+    return 1024;
+}
+
+int EssexEngine::Daemons::Config::ConfigDaemon::GetScreenHeight() {
+    return 768;
+}
---
+++ ConfigDaemon.h
@@ -0,0 +1,48 @@
+/* 
+ * Essex Engine
+ * 
+ * Copyright (C) 2017 Nathan Mentley - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the BSD license.
+ *
+ * You should have received a copy of the BSD license with
+ * this file. If not, please visit: https://github.com/nathanmentley/EssexEngine
+ */
+#pragma once
+
+#include <string>
+
+#include <EssexEngineCore/BaseDaemon.h>
+#include <EssexEngineConfigDaemon/IConfigDriver.h>
+
+namespace EssexEngine{ 
+namespace Daemons{
+namespace Config{
+	class ConfigDaemon:public BaseDaemon<IConfigDriver>
+	{
+		public:
+			ConfigDaemon(WeakPointer<Context> _context);
+			~ConfigDaemon();
+
+			void Init() {
+				if(GetContext()->HasDriver<Core::Logging::ILogDriver>()) {
+					GetContext()->GetDriver<Core::Logging::ILogDriver>()->LogLine(
+						"Loading Daemon [%s] [%s]",
+						GetDaemonName().c_str(),
+						GetDaemonVersion().c_str()
+					);
+			  	}
+			}
+			std::string GetDaemonName() { return "Config"; }
+			std::string GetDaemonVersion() { return ESSEX_ENGINE_VERSION; }
+
+			int GetBuildNumber();
+            std::string GetBuildName();
+        
+            int GetScreenWidth();
+            int GetScreenHeight();
+		private:
+			int build_number;
+			std::string build_name;
+	};
+}}};
